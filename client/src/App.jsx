777a45c7cbb6269bda6c088bbfff027a9876a954
prince mehta <154504461+CreatorguyS@users.noreply.github.com>
2025-10-20 13:30:18 +0530

    Userprovider wrapped in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,18 @@ import PackagePage from '@/pages/PackagePage'
 import OfferPage from  '@/pages/OfferPage'
 import TestPage from  '@/pages/TestPage'
 import Header from '@/components/Header'
+import { UserProvider } from './context/UserProvider'
+import Footer from '@/components/Footer'
+
 function App() {
   return (
-    <>
+    <UserProvider>
       <Header />
-      <PackagePage/>
-      <OfferPage/>
-      <TestPage/>
-    </>
+      <PackagePage />
+      <OfferPage />
+      <TestPage />
+      <Footer />
+    </UserProvider>
   )
 }
 
