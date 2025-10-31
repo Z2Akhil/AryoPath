d99f4ae92a2ec836747f578ab509e1b87bfa88d9
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-10-31 11:23:26 +0530

    add auth routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,26 +1,45 @@
 import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import AdminPanel from "./pages/AdminPanel";
+import LoginPage from "./pages/LoginPage";
 import PackageCatalog from "./components/catalog/Package.jsx";
 import OfferCatalog from "./components/catalog/Offer.jsx";
 import TestCatalog from "./components/catalog/Test.jsx";
 import HomePage from "./pages/HomePage";
 import ReportsPage from "./pages/ReportPage";
 import OrdersPage from "./pages/OrderPage";
-import SettingsPage from "./pages/SettingsPage";
+import AccountPage from "./pages/AccountPage";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AdminPanel />}>
-        <Route index element={<HomePage />} />
-        <Route path="home" element={<HomePage />} />
-        <Route path="reports" element={<ReportsPage />} />
-        <Route path="orders" element={<OrdersPage />} />
-        <Route path="packages" element={<PackageCatalog />} />
-        <Route path="offers" element={<OfferCatalog />} />
-        <Route path="tests" element={<TestCatalog />} />
-      </Route>
-      <Route path="settings" element={<SettingsPage />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<LoginPage />} />
+        
+        {/* Protected Routes */}
+        <Route 
+          path="/" 
+          element={
+            <ProtectedRoute>
+              <AdminPanel />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
+          <Route path="reports" element={<ReportsPage />} />
+          <Route path="orders" element={<OrdersPage />} />
+          <Route path="packages" element={<PackageCatalog />} />
+          <Route path="offers" element={<OfferCatalog />} />
+          <Route path="tests" element={<TestCatalog />} />
+          <Route path="account" element={<AccountPage />} />
+        </Route>
+        
+        <Route path="*" element={<LoginPage />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
