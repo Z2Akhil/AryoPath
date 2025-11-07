b289ecb2a5680e0e024074aa45919893a3804840
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-11-07 14:06:32 +0530

    add persistent logged in

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -6,3 +6,17 @@ export const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// Add request interceptor to include auth token automatically
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
